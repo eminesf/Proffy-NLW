cf2bc02feecebc685456584e2318fb155df8437e
Emiliano <emilianofuchs@gmail.com>
2020-08-06 01:07:34 -0300

    Adicionado imagens e fonte Poppins

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import './assets/styles/global.css';
 
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
+      <button type="button">
+        Clica em mim
+      </button>
     </div>
   );
 }
